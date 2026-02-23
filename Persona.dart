f652d21f1b65c46dfbfcc83f2bfade5b21ecbddd
JuanDJP2 <jotajotayeye34@gmail.com>
2026-02-23 14:31:30 -0500

    clase subida

--- Persona.dart
+++ Persona.dart
@@ -1,1 +1,27 @@
-Persona
+// Definimos una clase
+class Persona {
+  // Atributos (propiedades)
+  String nombre;
+  int edad;
+  String ciudad;
+
+  // Constructor
+  Persona(this.nombre, this.edad, this.ciudad);
+
+  // Método
+  void saludar() {
+    print("Hola, soy $nombre, tengo $edad años y vivo en $ciudad.");
+  }
+}
+
+void main() {
+  // Crear un objeto (instancia)
+  Persona persona1 = Persona("Juan", 25, "Madrid");
+
+  // Acceder a atributos
+  print(persona1.nombre);
+  print(persona1.edad);
+
+  // Llamar método
+  persona1.saludar();
+}
